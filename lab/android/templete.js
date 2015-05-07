a189198d5177ad22cea33f2637d80ce133c59638
Devonnuri <huiseongcomposer@gmail.com>
2015-05-07 20:45:44 +0900

    templete.js 추가
    이 templete.js에는 안드로이드 레퍼런스를 적용하기 전에의 기본 템플릿입니다.

---
+++ lab/android/templete.js
@@ -0,0 +1,19 @@
+var ctx = com.mojang.minecraftpe.MainActivity,currentMainActivity.get();
+
+function newLevel()
+{
+	ctx.runOnUiThread(new java.lang.Runnable(
+	{
+		run: function()
+		{
+			try
+			{
+				
+			}
+			catch(e)
+			{
+				print(e);
+			}
+		}
+	}));
+}
