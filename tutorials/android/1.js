5fc50c582543b6f5539e36bafbe822b42b875b34
Devonnuri <huiseongcomposer@gmail.com>
2015-05-12 15:02:25 +0900

    1.js / 안드로이드 템플릿 추가

--- tutorials/android/1.js
+++ tutorials/android/1.js
@@ -1,4 +1,19 @@
+var ctx = com.mojang.minecraftpe.MainActivity,currentMainActivity.get();
+
 function newLevel()
 {
-	print("Hello World");
+	ctx.runOnUiThread(new java.lang.Runnable(
+	{
+		run: function()
+		{
+			try
+			{
+				//Your code here! 당신의 코드를 입력해주세요^^
+			}
+			catch(e)
+			{
+				print(e);
+			}
+		}
+	}));
 }
