a589c6838d570174937d631255ad9e6881519f16
Devonnuri <devonnuri@gmail.com>
2015-05-14 21:59:14 +0900

    * unnamed.js 변경 | ctx 적용

---
+++ project/unnamed.js
@@ -0,0 +1,13 @@
+var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
+function newLevel() {
+    ctx.runOnUiThread(new java.lang.Runnable({
+        run: function () {
+            try {
+
+            }
+            catch (e) {
+                print(e);
+            }
+        }
+    }));
+}
